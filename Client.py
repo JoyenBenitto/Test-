119b093e29ad0e7a08cb42f3c5d4ddf9e3e4397a
JoyenBenitto <joyen.benitto12@gmail.com>
2022-09-22 14:21:18 +0530

    hello there here is a new file

--- Client.py
+++ Client.py
@@ -1,9 +1,9 @@
 from socket import*
-serv_addr='10.14.143.171'
+serv_addr='192.168.43.239'
 serv_port=7000
 client_sock=socket(AF_INET,SOCK_DGRAM)
 while 1:
-	msg='hello bois'
+	msg='hello advaithhh'
 	client_sock.sendto(msg.encode(),(serv_addr,serv_port))
 	mod_msg,s=client_sock.recvfrom(2048)
 	print("From Server:",mod_msg.decode())
